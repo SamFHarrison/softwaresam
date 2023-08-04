da8f599262891f5343449cded6091225c472a665
Sam F-Harrison <95258819+SamFHarrison@users.noreply.github.com>
2023-08-05 00:24:52 +0100

    Removing TailwindCSS (#1)
    * chore: refactor app without Tailwind
    
    * fix: excape apostrophe in <h2> tags
    
    * fix: change apostrophe to symbol in <h2> tags

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "SoftwareSam - Sam F-Harrison",
+  description: "Designed and built by Sam F-Harrison",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={openSans.className}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
