c403a63e1a78029636f649df8e48a04707153b22
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-01-20 02:17:29 +0000

    feat/wip: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,18 @@ import { Lato } from "next/font/google";
 import "./home.css";
 import Navbar from "@/components/Navbar/Navbar";
 
-const lato = Lato({ weight: ["100", "300", "400", "900"], subsets: ["latin"] });
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Sam F-Harrison",
   description: "Carefully crafted by me for you.",
+  authors: {
+    name: "Samuel Fletcher-Harrison",
+    url: "https://github.com/SamFHarrison",
+  },
 };
 
 export default function RootLayout({
