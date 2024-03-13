1c9466566a9f46fe9cb938d2acfed14cf391667c
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-03-13 16:08:26 +0000

    Add homepage title
    Added SamAI placeholder component
    Added hero title
    Refactored Navbar to be glassmorphic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="home-wrapper">
+      <div className="hero-title">
+        <h1>Hi.</h1>
+        <h1>I'm</h1>
+        <h1 className="orange">Sam</h1>
+      </div>
+      <div className="ai-sam"></div>
+    </div>
+  );
 }
