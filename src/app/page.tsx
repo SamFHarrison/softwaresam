aaa6d6ae827902c2ffc5289a210900d78ee4b5f1
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-03-14 10:42:20 +0000

    Escape apostrophe on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="home-wrapper">
       <div className="hero-title">
         <h1>Hi.</h1>
-        <h1>I'm</h1>
+        <h1>I&apos;m</h1>
         <h1 className="orange">Sam</h1>
       </div>
       <div className="ai-sam"></div>
