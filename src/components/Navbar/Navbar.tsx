646f4ea5990945f7a0a76cfaddb1c08de0c5660a
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-03-16 22:56:01 +0000

    Add paths to Navbar tabs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,22 +4,28 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import clsx from "clsx";
 import "./Navbar.css";
+import { useRouter } from "next/navigation";
 
 const tabs = [
-  { id: "projects", label: "Projects" },
-  { id: "home", label: "Home" },
-  { id: "articles", label: "Articles" },
+  { id: "projects", label: "Projects", path: "/projects" },
+  { id: "home", label: "Home", path: "/" },
+  { id: "articles", label: "Articles", path: "/articles" },
 ];
 
 export default function Navbar() {
   const [activeTab, setActiveTab] = useState(tabs[1].id);
 
+  const router = useRouter();
+
   return (
-    <div className="pill">
+    <nav className="pill">
       {tabs.map((tab) => (
         <button
           key={tab.id}
-          onClick={() => setActiveTab(tab.id)}
+          onClick={() => {
+            setActiveTab(tab.id);
+            router.push(tab.path);
+          }}
           className={clsx(activeTab === tab.id ? "" : "tab-active", "tab")}
         >
           {activeTab === tab.id && (
@@ -32,6 +38,6 @@ export default function Navbar() {
           {tab.label}
         </button>
       ))}
-    </div>
+    </nav>
   );
 }
