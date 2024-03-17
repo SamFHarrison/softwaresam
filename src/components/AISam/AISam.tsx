c181f38420b1a668c931901c7a822973572d53b0
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-03-17 10:12:30 +0000

    AISam placeholder

--- src/components/AISam/AISam.tsx
+++ src/components/AISam/AISam.tsx
@@ -1,20 +1,29 @@
 import React from "react";
-import "@/components/AISam/AISam.css";
+import "./AISam.css";
 import Arrow from "@/assets/Icons";
+import Image from "next/image";
+import avatar from "@/../public/imgs/avatar.png";
 
 function AISam() {
   return (
     <div className="glass-screen">
-      <div className="input-wrapper">
-        <input type="text" placeholder="Ask me anything..." />
+      <div className="ai-sam-placeholder">
+        <Image src={avatar} alt="Sam's avatar" />
+        <h2>AISam</h2>
+        <p>Coming June 24</p>
+      </div>
+
+      {/* <div className="input-wrapper">
+        <input type="text" placeholder="Ask me anything..." disabled />
         <button
           className="send-button"
           type="submit"
           aria-label="Send message button"
+          disabled
         >
           <Arrow />
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
