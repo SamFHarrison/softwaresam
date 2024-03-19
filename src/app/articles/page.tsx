19a573d5c3618dc0722766cb5eebe879a5673a29
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-03-19 23:46:39 +0000

    WIP: Article page
    Add sidebar divider
    Add sidebar hover effects
    Create transition variable

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
 
-function ArticlePage() {
-  return <section className="articles"></section>;
+import { RoutePaths } from "@/constants/constants";
+import { useRouter } from "next/navigation";
+import React from "react";
+import "./articles.css";
+import { Search } from "@/assets/Icons";
+
+function Article() {
+  const router = useRouter();
+  return (
+    <section className="articles-container glass-effect">
+      <div className="side-bar">
+        {/* // TODO: Search, Filter + Sort Feature */}
+        <ul className="article-sidebar">
+          <li onClick={() => router.push(RoutePaths.Articles)}>
+            <p className="">Article Title</p>
+            <p className="description">10th March &apos;24</p>
+          </li>
+        </ul>
+        <div className="divider" />
+        <div className="article-post">Hi Im an article</div>
+      </div>
+    </section>
+  );
 }
 
-export default ArticlePage;
+export default Article;
