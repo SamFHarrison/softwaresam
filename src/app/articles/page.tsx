7b0a892f323ca82bbed35875a53023026281289f
Sam F-Harrison <sgfletcherharrison@gmail.com>
2024-03-27 22:49:53 +0000

    feat: Implement ArticleSnippet in Articles

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -2,13 +2,16 @@
 
 import React from "react";
 import "./articles.css";
+import ArticleSnippet from "@/components/article/ArticleSnippet";
 
 function Article() {
   return (
     <section className="articles-container glass-effect">
       <div className="side-bar">
         {/* // TODO: Search, Filter + Sort Feature */}
-        <ul className="article-sidebar"></ul>
+        <ul className="article-sidebar">
+          <ArticleSnippet title="My first article" date="27th March '24" />
+        </ul>
       </div>
       <div className="divider" />
       <div className="article-post">
